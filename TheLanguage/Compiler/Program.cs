156fa170a8e827ce67344283c1563a8fc925e42f
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-07-15 10:22:18 +0200

    Added Token - Properties

--- TheLanguage/Compiler/Program.cs
+++ TheLanguage/Compiler/Program.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Reflection.Emit;
-
-namespace Compiler
+﻿namespace Compiler
 {
     class Program
     {
         static void Main(string[] args)
         {
-            DynamicMethod method = new DynamicMethod("Test", typeof(int), null);
-            var ilGenerator = method.GetILGenerator();
-
-            // return 1+4; ->
-            ilGenerator.Emit(OpCodes.Ldc_I4, 1); //1 wird auf Stack gelegt
-            ilGenerator.Emit(OpCodes.Ldc_I4, 4); //4 wird auf Stack gelegt
-            ilGenerator.Emit(OpCodes.Add); //Werte auf dem Stack (1 und 4) werden addiert
-            ilGenerator.Emit(OpCodes.Ret); //Returned den letzten Wert auf dem Stack
-
-            var func = (Func<int>)method.CreateDelegate(typeof(Func<int>));
-
-            Console.WriteLine(func());
-            Console.ReadLine();
+            // 1 + 1
         }
     }
 }
---
+++ TheLanguage/Compiler/Tokenizers/Token.cs
@@ -0,0 +1,13 @@
+﻿namespace Compiler.Tokenizers
+{
+    public class Token
+    {
+        public string Name { get; set; }
+
+        public int Position { get; set; }
+
+        public int Length { get; set; }
+
+        public string Value { get; set; }
+    }
+}
