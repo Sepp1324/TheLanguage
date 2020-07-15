363abe1fc83d01e47e70b3fcf000342bf7d9a51e
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-07-15 10:12:11 +0200

    Compiler: Basic IL - Code

---
+++ TheLanguage/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static string test;
+
+        static void Main(string[] args)
+        {
+            test = "Hello World";
+            Console.WriteLine(test);
+            Console.ReadLine();
+        }
+    }
+}
